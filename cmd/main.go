9ec1070bfed52e28603af85688fbb49966da7dc6
Ilya PC <mr.rusek99@gmail.com>
2021-04-02 14:24:31 +0600

    add service

--- cmd/main.go
+++ cmd/main.go
@@ -5,10 +5,16 @@ import (
 
 	"github.com/Yujiman/GoTodo"
 	"github.com/Yujiman/GoTodo/pkg/handler"
+	"github.com/Yujiman/GoTodo/pkg/repository"
+	"github.com/Yujiman/GoTodo/pkg/service"
 )
 
 func main() {
-	handler := new(handler.Handler)
+
+	rep := repository.NewRepository()
+	service := service.NewService(rep)
+	handler := handler.NewHandler(service)
+	
 	srv := new(TodoP.Server)
 	if err := srv.Run("8000", handler.InitRoutes()); err != nil {
 		log.Fatal("Error running  http server: ", err.Error())
--- pkg/handler/handler.go
+++ pkg/handler/handler.go
@@ -1,10 +1,16 @@
 package handler
 
 import (
+	"github.com/Yujiman/GoTodo/pkg/service"
 	"github.com/gin-gonic/gin"
 )
 
 type Handler struct {
+	service *service.Service
+}
+
+func NewHandler(service *service.Service) *Handler{
+	return &Handler{service: service}
 }
 
 func (h *Handler) InitRoutes() *gin.Engine {
