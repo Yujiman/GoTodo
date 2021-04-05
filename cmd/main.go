f2de96dfd3e461341327d4bb434c5442f07023bc
Ilya PC <mr.rusek99@gmail.com>
2021-04-05 14:45:30 +0600

    add  vim and  cofig file

--- cmd/main.go
+++ cmd/main.go
@@ -3,20 +3,32 @@ package main
 import (
 	"log"
 
-	"github.com/Yujiman/GoTodo"
+	TodoP "github.com/Yujiman/GoTodo"
 	"github.com/Yujiman/GoTodo/pkg/handler"
 	"github.com/Yujiman/GoTodo/pkg/repository"
 	"github.com/Yujiman/GoTodo/pkg/service"
+
+	"github.com/spf13/viper"
 )
 
 func main() {
+	if err := initConfig(); err != nil {
+		log.Fatalf("errors init configs: %s", err.Error())
+	}
 
 	rep := repository.NewRepository()
 	service := service.NewService(rep)
 	handler := handler.NewHandler(service)
-	
+
 	srv := new(TodoP.Server)
-	if err := srv.Run("8000", handler.InitRoutes()); err != nil {
+	if err := srv.Run(viper.GetString("port"), handler.InitRoutes()); err != nil {
 		log.Fatal("Error running  http server: ", err.Error())
 	}
+}
+
+func initConfig() error {
+	viper.AddConfigPath("config")
+	viper.SetConfigName("config")
+
+	return viper.ReadInConfig()
 }
